734536cd81761c4e2bf50f60303509d015b20d9b
melpon <melt@e-ml.jp>
2014-02-28 12:47:13 +0900

    fix shell for testing.

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-clang++ test.cpp -o test -std=c++11 -stdlib=libc++
+which clang++ > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  CC=clang++
+else
+  CC=g++
+fi
+$CC test.cpp -o test -std=c++11 -stdlib=libc++
 cat "test.html" | ./test
 rm test
