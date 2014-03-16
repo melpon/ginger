87cf1c6dbe3a4cb149fb82da1d46c4a59bceb82c
melpon <melt@e-ml.jp>
2014-03-16 11:30:09 +0900

    remove unnecessary brackets.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 which clang++ > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
+if [ $? -eq 0 ]; then
   clang++ test.cpp -o test -std=c++11 -stdlib=libc++
 else
   g++ test.cpp -o test -std=c++11
